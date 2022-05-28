7f4e31bd9f2635571129fbb342349bf3afe933e4
krmukesh1990 <106130632+krmukesh1990@users.noreply.github.com>
2022-05-28 13:38:19 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,31 @@
+
+
+provider"azurerm"{
+features {}
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-tf-acr"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = "mukeshacr777"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Standard"
+  admin_enabled            = false
+}
+resource "azurerm_container_registry" "acr1" {
+  name                     = "mukeshacr666"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Standard"
+  admin_enabled            = false
+}
+
+output "admin_password" {
+  value       = azurerm_container_registry.acr.admin_password
+  description = "The object ID of the user"
+}
